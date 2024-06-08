08b01aad029ab8f9616e23984f5c820b8589a7c8
alpha-lens <mathematic026@gmail.com>
2024-06-08 14:05:56 +0900

    JSX와 테일윈드 스타일을 위한 컴포넌트 분리 (재사용 가능성 향상)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 import NewProject from "./components/NewProject";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
